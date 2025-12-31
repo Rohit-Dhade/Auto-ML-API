a0d583b596a0fd160f7bb2be047a0e3a0e480702
Rohit-Dhade <rohitdhade99@gmail.com>
2025-12-31 11:37:18 +0530

    File uploading from FastApi is done

---
+++ app/main.py
@@ -0,0 +1,39 @@
+import os ,shutil
+from fastapi import FastAPI , File , UploadFile ,HTTPException
+from typing import Annotated
+from app.services.dataset import (DatasetCheck , InvalidDatasetError , DatasetAlreadyExists)
+
+dataset_service = DatasetCheck()
+app = FastAPI()
+
+upload_dir = 'storage/uploaded'
+os.makedirs(upload_dir , exist_ok=True)
+
+@app.get('/')
+def root():
+    return {'msg':"hello world from fastAPI"}
+
+@app.post('/uploadfile/')
+async def create_upload_file(file : UploadFile = File(...)):
+    try:
+        file_path = os.path.join(upload_dir , file.filename)
+        with open(file_path , 'wb') as buffer:
+            shutil.copyfileobj(file.file , buffer)
+
+            summary = dataset_service.UploadDatasets(filename = file.filename , filepath= file_path)
+
+            return {
+                "message":"File uploaded Successfully",
+                "dataset":summary
+            }
+
+    except InvalidDatasetError as e:
+        raise HTTPException(status_code=400 , detail=str(e))
+    
+    except DatasetAlreadyExists as e:
+        raise HTTPException(status_code=409 , detail=str(e))
+    
+    except Exception as e:
+        raise HTTPException(status_code=500 , detail="Interal server issue")
+            
+    
