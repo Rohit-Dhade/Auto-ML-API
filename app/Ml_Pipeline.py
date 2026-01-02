a20e38b32becab9567bcf99255fb11adc87511f7
Rohit-Dhade <rohitdhade99@gmail.com>
2026-01-02 10:42:53 +0530

    ML pipeline for Regression done.

--- app/Ml_Pipeline.py
+++ app/Ml_Pipeline.py
@@ -3,8 +3,9 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler , OneHotEncoder , LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score , recall_score , precision_score , f1_score
+from sklearn.metrics import accuracy_score , recall_score , precision_score , f1_score , mean_squared_error ,mean_absolute_error , r2_score
 
 def model_response_classification(dataFrame,  categorical_features , numerical_features , target):
 
@@ -58,4 +59,62 @@ def model_response_classification(dataFrame,  categorical_features , numerical_f
             "Recall": recall,
             "F1_score": f1,
             "labels":lable_mapping
+        }
+        
+        
+def model_response_regression(dataframe , categorical_features , numerical_features , target):
+        X = dataframe.drop(target , axis=1)
+        y = dataframe[target]
+        y_clean = dataframe[target].fillna(dataframe[target].mean())
+
+        # label = LabelEncoder()
+        # y_encoded = label.fit_transform(y)
+
+        # lable_mapping = {
+        #     int(i) : label
+        #     for i, label in enumerate(label.classes_)
+        # }
+
+        X_train , X_test , y_train , y_test = train_test_split(X , y_clean , test_size=0.2 , random_state=42)
+
+        numerical_transformer = Pipeline(steps=[
+            ('imputer' , SimpleImputer(strategy='mean')),
+            ('scaler' , StandardScaler())
+        ])
+
+        categorical_transformer = Pipeline(steps=[
+            ('imputer' , SimpleImputer(strategy='most_frequent')),
+            ('onehot' , OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num' , numerical_transformer , numerical_features),
+                ('cat' , categorical_transformer , categorical_features)
+            ]
+        )
+
+        model_pipeline = Pipeline(steps=[
+            ('preprocessor' , preprocessor),
+            ('regression' , LinearRegression())
+        ])
+
+        model_pipeline.fit(X_train , y_train)
+        y_pred = model_pipeline.predict(X_test)
+        
+        # r2_score = model_pipeline.score(X_test , y_test)
+        # accuracy = accuracy_score(y_test , y_pred)
+        # recall = recall_score(y_test , y_pred)
+        # precision = precision_score(y_test , y_pred)
+        # f1 = f1_score(y_test , y_pred)
+        
+        rmse = mean_squared_error(y_test , y_pred)
+        mae = mean_absolute_error(y_test , y_pred)
+        R2score = r2_score(y_test , y_pred)
+
+        return {
+            "model":"LinearRegressor",
+            "Root mean square error": rmse,
+            "Mean absolute error":mae,
+            "R2 score":R2score
         }
--- app/ModelTraining.py
+++ app/ModelTraining.py
@@ -2,37 +2,41 @@ import pandas as pd
 import numpy as np
 from app.services.dataset import InvalidDatasetError
 from fastapi import HTTPException
-from app.Ml_Pipeline import model_response_classification
+from app.Ml_Pipeline import model_response_classification , model_response_regression
 
-def modelTrainingfunc(requireds : dict):
-    
-    if(requireds["problem_type"] == 'classification'):
-        try:
-            df = pd.read_csv(f"storage/datasets/{requireds['filename']}")
-            cat_columns = []
-            numerical_columns = []
-            target_col = requireds["target_column"]
 
-            for col in list(df.columns):
-                if col == target_col:
-                    continue
+def modelTrainingfunc(requireds: dict):
 
-                if str(df[col].dtype) == "object":
-                   cat_columns.append(col)
+    try:
+        df = pd.read_csv(f"storage/datasets/{requireds['filename']}")
+        cat_columns = []
+        numerical_columns = []
+        target_col = requireds["target_column"]
 
-                else:
-                    numerical_columns.append(col)
+        for col in list(df.columns):
+            if col == target_col:
+                continue
 
-            response = model_response_classification(df , cat_columns , numerical_columns , target_col)
+            if str(df[col].dtype) == "object":
+                cat_columns.append(col)
 
-            # return requireds
-            return response
+            else:
+                numerical_columns.append(col)
+    except InvalidDatasetError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-        except InvalidDatasetError as e:
-            raise HTTPException(status_code=400 , detail=str(e))
+    if requireds["problem_type"] == "classification":
 
-    elif (requireds["problem_type"] == 'regression'):
-        pass
+        classification_response = model_response_classification(
+            df, cat_columns, numerical_columns, target_col
+        )
+        return classification_response
+
+    elif requireds["problem_type"] == "regression":
+        regression_response = model_response_regression(df , cat_columns , numerical_columns , target_col)
+        return regression_response
 
     else:
-        return {'msg':"No proper problem type. Please select between regression and classification"}
+        return {
+            "msg": "No proper problem type. Please select between regression and classification"
+        }
