7e3ddff91b2ea5090d8cf2904587cc597fde30f7
Rohit-Dhade <rohitdhade99@gmail.com>
2026-01-11 00:01:29 +0530

    CorsMiddleware updated.

--- Backend/main.py
+++ Backend/main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://auto-ml-api.vercel.app"],
+    allow_origins=["https://auto-ml-api.vercel.app" , "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
