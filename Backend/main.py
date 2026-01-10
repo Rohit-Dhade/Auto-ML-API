feb3c54f703613b723d4a99a0185f66491959348
Rohit-Dhade <rohitdhade99@gmail.com>
2026-01-10 22:22:28 +0530

    Changed.

--- Backend/main.py
+++ Backend/main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://humble-appreciation-production-8bc7.up.railway.app"],
+    allow_origins=["http://http://localhost:5173/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
